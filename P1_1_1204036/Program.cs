5d7a154a0473dd780b1a24676ebfbb46f23eecd5
nawafnaofal <nawafnaufal1@gmail.com>
2021-11-05 21:28:41 +0700

    Push pertama

---
+++ P1_1_1204036/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_1_1204036
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World From Visual Studio, I'm 1204036");
+        }
+    }
+}
